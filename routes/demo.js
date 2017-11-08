4ca5028ced55136d6812acddb66d58a9aa3cbaea
tangcc <tangchencreator@126.com>
2017-11-08 17:45:40 +0800

    项目初始化

---
+++ routes/demo.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var request = require('request');
+var router = express.Router();
+var api = "http://www.vccdd.loan:3000/tushareapi/realtimeboxoffice";
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    res.render('demo/index', { title: 'webtushare', subtitle: 'tushare的接口实现' });
+});
+
+router.get('/realtime', function(req, res, next){
+    request(api, function(error, response, body) {
+        if (!error && response.statusCode == 200) {
+          console.log(body)
+          res.render('demo/list', {movices: JSON.parse(body)});
+        }
+    })
+   
+});
+
+module.exports = router;
