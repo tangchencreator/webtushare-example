b347b9dd1c5df2cc8cccce953340b35c3e1f9be8
tangchenchen <tcc@localhost.localdomain>
2017-11-09 22:59:36 +0800

    每日票房接口

--- routes/demo.js
+++ routes/demo.js
@@ -1,17 +1,27 @@
 var express = require('express');
 var request = require('request');
 var router = express.Router();
-var api = "http://localhost:4000/tushareapi/realtimeboxoffice";
+var api = "http://localhost:4000/tushareapi/";
 /* GET home page. */
 router.get('/', function(req, res, next) {
     res.render('demo/index', { title: 'webtushare', subtitle: 'tushare的接口实现' });
 });
 
 router.get('/realtime', function(req, res, next){
-    request(api, function(error, response, body) {
+    request(api + "realtimeboxoffice", function(error, response, body) {
         if (!error && response.statusCode == 200) {
           console.log(body)
-          res.render('demo/list', {movices: JSON.parse(body)});
+          res.render('demo/realtime', {movices: JSON.parse(body)});
+        }
+    })
+   
+});
+
+router.get('/dayboxoffice', function(req, res, next){
+    request(api + "dayboxoffice", function(error, response, body) {
+        if (!error && response.statusCode == 200) {
+          console.log(body)
+          res.render('demo/day', {movices: JSON.parse(body)});
         }
     })
    
